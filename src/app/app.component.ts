02bc1f625bb6a7e3de8e7d458d6b7010191d0332
achl93 <andy_ch_009@hotmail.com>
2017-11-18 21:30:46 -0800

    finished functionality and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,10 @@ interface Post {
   content: string;
 }
 
+interface PostId extends Post { 
+  id: string; 
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,12 +22,36 @@ interface Post {
 
 export class AppComponent {
 
+  title: string;
+  content: string;
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
   postsCol: AngularFirestoreCollection<Post>;
-  posts: Observable<Post[]>;
+  posts: any;
+
+  addPost() {
+    this.afs.collection('posts').doc('my-custom-id').set({'title': this.title, 'content': this.content});
+  }
+
+  getPost(postId) {
+    this.postDoc = this.afs.doc('posts/'+postId);
+    this.post = this.postDoc.valueChanges();
+  }
+
+  deletePost(postId) {
+    this.afs.doc('posts/'+postId).delete();
+  }
 
   constructor(private afs: AngularFirestore) {}
   ngOnInit() {
     this.postsCol = this.afs.collection('posts');
-    this.posts = this.postsCol.valueChanges();
+    this.posts = this.postsCol.snapshotChanges()
+    .map((actions) => {
+      return actions.map((a) => {
+        const data = a.payload.doc.data() as Post;
+        const id = a.payload.doc.id;
+        return { id, data };
+      });
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -21,7 +22,8 @@ const firebaseConfig = {
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(firebaseConfig),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
